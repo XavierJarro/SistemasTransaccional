5fd0eaceb4d8b403410a38867c49176e1760bd3f
XavierJarro <xa_cx4_vi@hotmail.com>
2020-12-08 11:57:48 -0500

    agregar documentacion

--- src/main/java/ec/edu/ups/utils/Resources.java
+++ src/main/java/ec/edu/ups/utils/Resources.java
@@ -12,7 +12,7 @@ import javax.sql.DataSource;
 
 public class Resources {
 
-    String dsName = "java:jboss/datasources/sistemaTransaccionalDS";
+    String dsName = "java:jboss/datasources/sistemaTranDS";
 
     @Produces
     private Connection createConnection() throws SQLException, NamingException {
